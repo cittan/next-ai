b6ef1c33deebfb368ff3380d2b17b6ecad183164
cittan <3198001545@qq.com>
2026-07-13 21:31:34 +0800

    完善前端对后端相关文档增删改操作的调用

---
+++ frontend/lib/admin.ts
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @param url 
+ * @param opts
+ * {method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ key: 'value' })} 
+ */
+const base = (url: string, opts: RequestInit) => {
+    const token = localStorage.getItem('token');
+    const headers: any = { ...opts.headers };
+    if(token) {
+        headers['Authorization'] = `Bearer ${token}`;
+    }
+    return fetch(url, { ...opts, headers }).then(r => r.ok ? r.json() : r.json().then(error => {throw new Error(error)}));
+}
+
+export const adminApi = {
+    uploadDocument: (fd: FormData) => base('/api/manage/document/upload', { method: 'POST', body: fd }),
+    listDocument: (p: { keyword: string, pageNo: number, pageSize: number }) => {
+        const params = new URLSearchParams({
+            keyword: p.keyword,
+            page: p.pageNo.toString(),
+            pageSize: p.pageSize.toString()
+        });
+        return base(`/api/manage/document/page/query?${params}`, { method: 'GET' });
+    },
+    deleteDocument: (id: string) => base(`/api/manage/document/delete`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ documentId: id }) }),
+}
