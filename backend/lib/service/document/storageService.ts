a9cd6f27c55f40d1722cfe543fc72f9619d858b7
cittan <3198001545@qq.com>
2026-07-13 17:42:37 +0800

    创建Minio存储服务，包含文件上传，获取文件下载URL，删除文件功能

---
+++ backend/lib/service/document/storageService.ts
@@ -0,0 +1,19 @@
+import { config } from "@/lib/config";
+import { getMinio } from "@/lib/db/minio";
+
+
+export async function uploadFile(buffer: Buffer, file: File, mimeType: string) {
+    // 实现文件上传逻辑
+    const objectName = `${config.minio.objectPrefix}/${Date.now()}_${file.name}`;
+    await getMinio().putObject(config.minio.bucketName, objectName, buffer, buffer.length, { contentType: mimeType });
+    return objectName;
+}
+
+//获取文件下载URL,有效期7天
+export async function getDownloadUrl(objectName: string): Promise<string> {
+    return getMinio().presignedGetObject(config.minio.bucketName, objectName, 7 * 24 * 60 * 60);
+}
+
+export async function deleteFile(objectName: string) {
+    await getMinio().removeObject(config.minio.bucketName, objectName);
+}
